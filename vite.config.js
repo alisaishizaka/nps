2722268d4587f5a8e68bc1032fa73cc5c45ba18b
alisaishizaka <ish24001@byui.edu>
2026-05-23 22:19:29 +0900

    5

--- vite.config.js
+++ vite.config.js
@@ -11,8 +11,11 @@ export default defineConfig({
     outDir: "../dist",
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "src/index.html")
+        main: resolve(__dirname, "src/index.html"),
+        conditions: resolve(__dirname, "src/conditions.html")
       }
     }
   }
 });
+
+
