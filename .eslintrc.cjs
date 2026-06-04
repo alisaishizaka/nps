638e18ff0f9586becf9ed0429037d7137cfb0db6
alisaishizaka <ish24001@byui.edu>
2026-06-04 18:19:33 +0900

    7

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es2024: true,
+  },
+  extends: ["eslint:recommended"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    // You can add project-specific rules here.
+  },
+};
